6da349893289a9c87ff2c64b7e853ef5537fd6b5
Patricia Decker <patriciasquared@gmail.com>
2016-01-21 07:04:38 -0800

    spiral print 4/5 test cases passing

--- warmup3.py
+++ warmup3.py
@@ -8,6 +8,7 @@ def spiral_print(matrix):
     direction: 0 first row, 1 last col, 2 last row, 3 first col
     """
     direction = 0
+    result = ''
     while matrix:
         n = len(matrix)
         m = len(matrix[0])
@@ -17,14 +18,16 @@ def spiral_print(matrix):
         if action == 0:
             # print first row
             for item in matrix[0]:
-                print item
+                # print item
+                result = result + str(item) + ','
             # new matrix lops off first row
             matrix = matrix[1:]
         
         elif action == 1:
             # print last col
             for row in matrix:
-                print row[-1]
+                # print row[-1]
+                result = result + str(row[-1]) + ','
             # new matrix lops off last col
             new_matrix = []
             for row in matrix:
@@ -35,16 +38,29 @@ def spiral_print(matrix):
         elif action == 2:
             # print last row, in reverse order
             row = matrix[-1]
-            for i in range(-1, -1 * len(row) - 1, -1)
-                print row[i]
+            for i in range(-1, -1 * len(row) - 1, -1):
+                # print row[i]
+                result = result + str(row[i]) + ','
             # new matrix lops off last row
             matrix = matrix[:-1]
-            
-        else: # action == 3
 
+        else: # action == 3
+            # print first col, in reverser order
+            for i in range(-1, -1 * len(matrix) - 1, -1):
+                # print matrix[i][0]
+                result = result + str(matrix[i][0]) + ','
+            # new matrix lops off first col
+            new_matrix = []
+            for row in matrix:
+                new_row = row[1:]
+                new_matrix.append(new_row)
+            matrix = new_matrix
+                
         direction += 1
-        
-        # make new matrix
+    
+    # strip off final comma
+    print result[:-1]
+    return 
 
     # 0
 # parse input
@@ -54,4 +70,6 @@ matrix = []
 for i in range(n):
     row = map(int,raw_input().strip().split(','))
     matrix.append(row)
+
+spiral_print(matrix)
 
